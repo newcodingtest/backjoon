87863ce5a7443cf7deef33fae7351da2908bede8
jy.YOON <57785267+newcodingtest@users.noreply.github.com>
2020-05-22 19:17:38 +0900

    Add files via upload

---
+++ 백준 11단계-재귀/11-2juyoung.java
@@ -0,0 +1,30 @@
+package backjoon;
+import java.util.Scanner;
+
+public class fab {
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("입력하시오");
+		int i=sc.nextInt();
+		int fab=0;
+		
+	   for(int k=fab; k<=i; ++k) {
+			fab=fab+k;
+		
+			}
+	   if(i>20) {System.out.println("초과");}
+	   else {System.out.println(fab);}
+	
+		}
+
+	}
+
+
+		
+		
+		
+		
+	
+
+
+
