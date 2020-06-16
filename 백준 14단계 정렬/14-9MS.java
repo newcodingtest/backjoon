45baa8751320d11776946af4f97601c7f345e595
sktior <42050824+sktior@users.noreply.github.com>
2020-06-16 12:33:34 +0900

    Update 14-9MS.java

--- 백준 14단계 정렬/14-9MS.java
+++ 백준 14단계 정렬/14-9MS.java
@@ -6,7 +6,7 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.Arrays;
 import java.util.StringTokenizer;
-
+//55056KB, 648ms
 class Person implements Comparable<Person>{
 	int age;
 	String name;
