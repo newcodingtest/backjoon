80ef729b2811326ab3f3326f74c4b5c4b1d86ff1
sktior <42050824+sktior@users.noreply.github.com>
2020-07-22 20:00:09 +0900

    Update 16-7MS.java

--- 백준 16단계 동적계획법/16-7MS.java
+++ 백준 16단계 동적계획법/16-7MS.java
@@ -9,17 +9,19 @@ public class Main {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		int n = Integer.parseInt(br.readLine());
-		int[] arr = new int[n];
-		for(int i=0; i<n; i++) {
-			arr[i] = Integer.parseInt(br.readLine());
+		int[] arr = new int[n+1];
+		int[] tmp = new int[n+1];
+		for(int i=1; i<=n; i++) {
+			tmp[i] = Integer.parseInt(br.readLine());
 		}
-		int sum = arr[0];
-		for(int i=0; i<arr.length-1; i++) {
-			if(arr[i+1] > arr[i]) {
-				sum += arr[i+1];
-			}
+		arr[0] = 0;
+		tmp[0] = 0;
+		arr[1] = tmp[1];
+		arr[2] = Math.max(arr[1] + tmp[2], tmp[2]);
+		for(int i=3; i<=n; i++) {
+			arr[i] = Math.max(arr[i-2]+tmp[i] , tmp[i-1] + arr[i-3] + tmp[i]);
 		}
-		bw.write(String.valueOf(sum));
+		bw.write(arr[n] + "\n");
 		bw.flush();
 		bw.close();
 	}
