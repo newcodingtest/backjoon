47fb4d0ee0551201f23757d4350fcd3d8a56ed02
sktior <42050824+sktior@users.noreply.github.com>
2020-08-19 14:48:03 +0900

    Add files via upload

---
+++ 백준 18단계 그리디 알고리즘/18-3MS.java
@@ -0,0 +1,42 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st = null;
+		int num = Integer.parseInt(br.readLine());
+		int arr[] = new int[num];
+		int result = 0;
+		int tmp = 0;
+		st = new StringTokenizer(br.readLine());
+		for(int i=0; i<arr.length; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		swap(arr);
+		for(int i=0; i<arr.length; i++) {
+			tmp += arr[i];
+			result += tmp;
+		}
+		bw.write(result + "\n");
+		bw.flush();
+		bw.close();
+		
+	}
+	public static void swap(int[] arr) {
+		for(int i=0; i<arr.length-1; i++) {
+			for(int j=i+1; j<arr.length; j++) {
+				if(arr[i] > arr[j]) {
+					int tmp = arr[i];
+					arr[i] = arr[j];
+					arr[j] = tmp;
+				}
+			}
+		}
+	}
+}
